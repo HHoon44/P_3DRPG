8a8f156109c499a186178577fa0c7f018e5bde4f
TalesShop <hvh6634@naver.com>
2022-03-17 15:16:53 +0900

    3월17일

--- My 3D RPG/Assets/Script/UI/UIWindow.cs
+++ My 3D RPG/Assets/Script/UI/UIWindow.cs
@@ -8,19 +8,19 @@ using UnityEngine;
 namespace ProjectChan.UI
 {
     /// <summary>
-    /// => 조그마한 팝업, UI내의 UIElement를 제외한 모든 UI의 베이스 클래스
-    /// => 모든 UI들은 이 클래스를 갖는다
+    /// 모든 UI의 베이스 클래스 ( 조그마한 팝업, UI내의 UIElement를 제외 )
     /// </summary>
     [RequireComponent(typeof(CanvasGroup))]
     public class UIWindow : MonoBehaviour
     {
-        public bool isOpen;     // -> 해당 UI의 활성화 상태
+        // public 
+        public bool isOpen;     // 해당 UI의 활성화 상태
 
         private CanvasGroup cachedCanvasGroup;
 
         /// <summary>
-        /// => 캔버스 그룹을 통해 UI를 활성 or 비활성화 하는 효과를 준다(알파값을 설정)
-        /// => 비활성화 시 실제로 객체가 비활성화되는 것이 아니므로 UI입력을 차단 한다(블록 레이캐스트)
+        /// 캔버스 그룹을 이용하여, UI를 활성화/비활성화 하는 효과를 준다 ( 알파값을 설정 )
+        /// 비활성화 시, 실제로 객체가 비활성화 되는 것이 아니라 UI의 입력을 차단 한다 ( 블록 레이캐스트 )
         /// </summary>
         public CanvasGroup CachedCanvasGroup
         {
@@ -39,7 +39,7 @@ namespace ProjectChan.UI
         }
 
         /// <summary>
-        /// => UI 초기화 기능 메서드
+        /// UI를 초기화 하는 메서드
         /// </summary>
         public virtual void InitWindow()
         {
@@ -56,7 +56,7 @@ namespace ProjectChan.UI
         }
 
         /// <summary>
-        /// => UI를 활성화 하는 기능 메서드
+        /// UI 활성화 메서드
         /// </summary>
         /// <param name="force"> 활성화 여부 </param>
         public virtual void Open(bool force = false)
@@ -70,7 +70,7 @@ namespace ProjectChan.UI
         }
 
         /// <summary>
-        /// => UI를 비활성화 하는 기능 메서드
+        /// UI 비활성화 메서드
         /// </summary>
         /// <param name="force"> 비활성화 여부 </param>
         public virtual void Close(bool force = false)
@@ -84,13 +84,18 @@ namespace ProjectChan.UI
         }
 
         /// <summary>
-        /// => 활성화 상태에 따라 캔버스 그룹 내의 필드를 설정
+        /// 활성화 여부에 따라, 캔버스 그룹의 필드를 설정하는 메서드
         /// </summary>
         /// <param name="isActive"></param>
         private void SetCanvasGroup(bool isActive)
         {
+            // 불투명도 설정
             CachedCanvasGroup.alpha = Convert.ToInt32(isActive);
+
+            // 입력을 받을지에 대한 여부 설정
             cachedCanvasGroup.interactable = isActive;
+
+            // 레이캐스트를 위한 콜라이더로 적용할지에 대한 여부
             cachedCanvasGroup.blocksRaycasts = isActive;
         }
     }
--- My 3D RPG/Assets/Script/UI/UIWindowManager.cs
+++ My 3D RPG/Assets/Script/UI/UIWindowManager.cs
@@ -9,61 +9,64 @@ using UnityEngine;
 namespace ProjectChan.UI
 {
     /// <summary>
-    /// => 모든 UIWindow클래스를 관리 하는 매니저 클래스
+    /// 모든 UIWindow클래스를 관리 하는 매니저 클래스
     /// </summary>
     public class UIWindowManager : Singleton<UIWindowManager>
     {
         /// <summary>
-        /// => UM에 등록된 UIWindow를 갖는 객체 리스트
+        /// UM에 등록된 UIWindow를 담아놓을 객체 리스트
         /// </summary>
         private List<UIWindow> totalUIWindows = new List<UIWindow>();
 
         /// <summary>
-        /// => 활성화 되어있는 UIWindow를 갖는 객체 리스트
+        /// 활성화 상태의 UIWindow를 갖는 객체 리스트
         /// </summary>
         private List<UIWindow> totalOpenWindows = new List<UIWindow>();
 
         /// <summary>
-        /// => UM에 UIWindow 등록 시 캐싱하여 담아둘 딕셔너리
+        /// UM에 UIWindow 등록 시, 캐싱하여 담아둘 딕셔너리
         /// </summary>
         private Dictionary<string, UIWindow> cachedTotalUIWindowDic = new Dictionary<string, UIWindow>();
 
         /// <summary>
-        /// => UM에 등록된 UI에 인스턴스 접근 시 해당 인스턴스 들을 캐싱하여 담아둘 딕셔너리
+        /// UM에 등록된 UI에 인스턴스 접근 시, 해당 인스턴스를 캐싱하여 담아둘 딕셔너리
+        /// ( UM를 이용해서 특정 인스턴스 접근 메서드를 사용할 시에, 내가 코드로 직접 접근한 UIWindow들만 담김 )
         /// </summary>
         private Dictionary<string, object> cachedInstanceDic = new Dictionary<string, object>();
 
+        /// <summary>
+        /// UM 초기화 메서드
+        /// </summary>
         public void Initialize()
         {
             InitAllWindow();
         }
 
         /// <summary>
-        /// => UIWindow를 담아놓는 메서드
+        /// UIWindow를 담아놓는 메서드
         /// </summary>
         /// <param name="uiWindow"> 담아둘 UIWindow </param>
         public void AddTotalWindow(UIWindow uiWindow)
         {
-            // -> 키 이름은 UI의 이름으로 합니다!
+            // Key값은 UI의 이름
             var key = uiWindow.GetType().Name;
 
             bool hasKey = false;
 
-            // -> 리스트와 딕셔너리에 등록하고자 하는 인스턴스가 있다면!
+            // 리스트와 딕셔너리에 등록하려는 UI가 존재한다면
             if (totalUIWindows.Contains(uiWindow) || cachedTotalUIWindowDic.ContainsKey(key))
             {
-                // -> 이미 캐싱 해놓은 인스턴스가 있다면!
+                // 저장하려는 UI가 이미 캐싱이 되어있다면
                 if (cachedTotalUIWindowDic[key] != null)
                 {
                     return;
                 }
                 else
                 {
-                    // -> 키 있습니다!
+                    // Key가 존재
                     hasKey = true;
 
-                    // -> 키 값은 있으나 비어있으므로 참조하고 있는 인스턴스가 없어
-                    //    리스트에서 제거 합니다!
+                    // 키 값은 있으나, 참조하고 있는 인스턴스가 없으므로 리스트에서 제거
                     for (int i = 0; i < totalUIWindows.Count; i++)
                     {
                         if (totalUIWindows[i] == null)
@@ -74,24 +77,23 @@ namespace ProjectChan.UI
                 }
             }
 
-            // -> 리스트에 현재 인스턴스할 UIWindow를 저장합니다!
+            // 인스턴스할 UI를 저장한다
             totalUIWindows.Add(uiWindow);
 
-            // -> 키가 있다면!
+            // 키가 있다면
             if (hasKey)
             {
-                // -> 캐싱 딕셔너리에 저장합니다!
+                // 인스턴스 값을 인스턴스 딕셔너리에 저장
                 cachedInstanceDic[key] = uiWindow;
             }
             else
             {
-                // -> 키 값과 같이 저장합니다!
                 cachedTotalUIWindowDic.Add(key, uiWindow);
             }
         }
 
         /// <summary>
-        /// => 활성화 할 UIWindow를 리스트에 추가하는 메서드
+        /// 활성화 된 UIWindow를 리스트에 추가하는 메서드
         /// </summary>
         /// <param name="uiWindow"> 활성화 할 UIWindow </param>
         public void AddOpenWindow(UIWindow uiWindow)
@@ -103,7 +105,7 @@ namespace ProjectChan.UI
         }
 
         /// <summary>
-        /// => 비활성화 할 UIWIndow를 리스트에서 제거하는 메서드
+        /// 비활성화 된 UIWIndow를 리스트에서 제거하는 메서드
         /// </summary>
         /// <param name="uiWindow"> 비활성화 할 UIWindow </param>
         public void RemoveOpenWindow(UIWindow uiWindow)
@@ -115,29 +117,30 @@ namespace ProjectChan.UI
         }
 
         /// <summary>
-        /// => UM에 등록된 UIWindow 인스턴스를 반환하는 메서드
+        /// UM에 등록된 UIWindow 인스턴스를 반환하는 메서드
         /// </summary>
         /// <typeparam name="T"> 반환 받을 UIWindow의 타입 </typeparam>
         /// <returns></returns>
-        public T GetWindow<T>()
-            where T : UIWindow
+        public T GetWindow<T>() where T : UIWindow
         {
+            // T 타입으로 Key 값을 설정
             string key = typeof(T).Name;
 
+            // Key 값이 캐싱 Dic에 없다면
             if (!cachedTotalUIWindowDic.ContainsKey(key))
             {
                 return null;
             }
 
-            // -> 인스턴스 시 담아둘 딕셔너리에 키 값이 없다면!
+            // 인스턴스 시 담아둘 딕셔너리에 키 값이 없다면
             if (!cachedInstanceDic.ContainsKey(key))
             {
-                // -> 딕셔너리에 추가 해줍니다!
+                // 값이 지정된 개체와 동일한 지정된 형식의 개체를 Key값과 Dic에 저장
                 cachedInstanceDic.Add(key, (T)Convert.ChangeType(cachedTotalUIWindowDic[key], typeof(T)));
             }
             else if (cachedInstanceDic[key].Equals(null))
             {
-                // -> 키 값은 존재하나 Null 이라면 UIWindow 인스턴스를 T타입으로 캐스팅 후 등록합니다!
+                // 값이 지정된 개체와 동일한 지정된 형식의 개체를 Dic에 저장
                 cachedInstanceDic[key] = (T)Convert.ChangeType(cachedTotalUIWindowDic[key], typeof(T));
             }
 
@@ -145,7 +148,7 @@ namespace ProjectChan.UI
         }
 
         /// <summary>
-        /// => UM에 등록된 모든 UIWindow를 초기화 하는 기능
+        /// UM에 등록된 모든 UIWindow를 초기화 하는 메서드
         /// </summary>
         public void InitAllWindow()
         {
@@ -157,41 +160,5 @@ namespace ProjectChan.UI
                 }
             }
         }
-
-        #region 안씀
-
-        /// <summary>
-        /// => 현재 활성화 된 UIWindow중 가장 위의 UIWindow를 반환 하는 메서드
-        /// </summary>
-        /// <returns></returns>
-        public UIWindow GetTopWindow()
-        {
-            // -> 최상위 UIWindow를 찾는 작업입니다!
-            for (int i = 0; i < totalUIWindows.Count - 1; i++)
-            {
-                if (totalUIWindows[i] != null)
-                {
-                    return totalUIWindows[i];
-                }
-            }
-
-            return null;
-        }
-
-        /// <summary>
-        /// => UM에 등록된 모든 UIWindow를 닫는 메서드
-        /// </summary>
-        public void CloseAllUI()
-        {
-            for (int i = 0; i < totalUIWindows.Count; i++)
-            {
-                if (totalUIWindows[i] != null)
-                {
-                    totalUIWindows[i].Close(true);
-                }
-            }
-        }
-
-        #endregion
     }
 }
